14e6439198a0c97a3c703d374a715ba02ba0dc0f
Shivane Rana <shivanerana2004@gmail.com>
2024-05-25 23:48:03 +0530

    few stuff added

---
+++ script.js
@@ -0,0 +1,4 @@
+const clear = document.querySelector("clear");
+const resize = document.querySelector("resize");
+const rgb = document.querySelector("rgb");
+const center = document.querySelector("#center");
